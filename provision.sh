5595a139e6a0d0f72b12989a142db722c770ed1c
Benjamin P Lee <benjaminplee@gmail.com>
2014-12-02 21:06:16 -0600

    Setup a few core things

--- provision.sh
+++ provision.sh
@@ -3,4 +3,8 @@
 cp -r /vagrant/dotfiles/.??* ~/
 
 sudo apt-get update
+sudo apt-get install git -y
+
+# Increase file handle limits
+sudo su -c 'echo -e "vagrant soft nofile 4096\nvagrant hard nofile 65536\n" > /etc/security/limits.d/vagrant.user.limits.conf'
 
