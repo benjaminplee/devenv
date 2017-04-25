92f683ac2807a910a24b7237e07932e97f297d2c
Benjamin P Lee <benjaminplee@gmail.com>
2017-04-25 11:31:53 -0500

    Minimal install with elixir

--- provision.sh
+++ provision.sh
@@ -2,6 +2,10 @@
 
 cp -r /vagrant/dotfiles/.??* ~/
 
+# Elixir / Erlang Repositories
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update -y
+
 sudo apt-get install git -y
 
 # Install Vundle (Vim package manager) and activate
@@ -11,3 +15,5 @@ vim -i NONE +PluginInstall +qall > /dev/null 2>&1
 # Increase file handle limits
 sudo su -c 'echo -e "vagrant soft nofile 4096\nvagrant hard nofile 65536\n" > /etc/security/limits.d/vagrant.user.limits.conf'
 
+# Install Elixir and Erlang
+sudo apt-get install esl-erlang elixir -y
